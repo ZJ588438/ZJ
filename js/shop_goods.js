687c55ddc17617d09e1cfd2798fe14ac5f8e07e6
ZJ <1192234018@qq.com>
2020-06-04 09:22:35 +0800

    the first commit

---
+++ js/shop_goods.js
@@ -0,0 +1,21 @@
+/**
+ * Goods Page Js
+ */
+ jQuery(function(){
+ 	jQuery("#good_num_jian").click(function(){
+ 		var num = jQuery("#good_nums").val();
+ 		num = parseInt(num);
+ 		num = num-1;
+ 		if(num<=1){
+ 			num = 1;
+ 		}
+ 		jQuery("#good_nums").val(num);
+ 	});
+
+ 	jQuery("#good_num_jia").click(function(){
+ 		var num = jQuery("#good_nums").val();
+ 		num = parseInt(num);
+ 		num = num+1;
+ 		jQuery("#good_nums").val(num);
+ 	});
+ });
